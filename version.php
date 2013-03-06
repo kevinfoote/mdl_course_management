24c682ee260f13583e9ddee6ce766df3520269d1
Kevin P. Foote <kpfoote@iup.edu>
2013-03-06 10:32:51 -0500

    Update Moodle version requirement

--- version.php
+++ version.php
@@ -9,5 +9,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013020100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->requires  = 2012120300.00;     // Requires this Moodle version
 $plugin->component = 'block_course_management'; // Full name of the plugin (used for diagnostics)
