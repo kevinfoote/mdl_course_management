0ece2483825f75d25435de61955f3dc42bde1697
Kevin P. Foote <kpfoote@iup.edu>
2013-03-14 11:40:11 -0400

    Form feedback testing

--- cm.php
+++ cm.php
@@ -25,9 +25,45 @@ $site = get_system_context();
 if ($cm->is_cancelled()) {
     // cancelled forms redirect back to /my/
     redirect("$CFG->wwwroot/my/");
-} else if ($fromselect = $cm->get_data()) { 
+} else if ($data = $cm->get_data()) { 
     // DO data processing etc..
-    //$fromselect->
+    $blockname = course_management::_s('blockname');
+    $header = course_management::_s('cmcreate');
+
+    // setup page components 
+    $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+    $PAGE->navbar->add(get_string('myhome'), new moodle_url('/my/'));
+    $PAGE->navbar->add($blockname);
+    $PAGE->set_title($blockname . ': '. $header);
+    $PAGE->set_heading($blockname . ': '.$header);
+    $PAGE->set_url('/blocks/course_management/cm.php');
+    $PAGE->set_pagelayout('standard');
+
+
+    // draw page to screen
+    echo $OUTPUT->header();
+    echo $OUTPUT->heading($blockname);
+    // CONTENT
+    echo '<p>Hello '.$USER->firstname.' '.$USER->lastname.' ('.$USER->username.').</p>';
+    echo '<p>Use the below form to select a term and course to create.</p>';
+
+//    foreach ($warnings as $type => $warning) {
+//        $class = ($type == 'success') ? 'notifysuccess' : 'notifyproblem';
+//        echo $OUTPUT->notification($warning, $class);
+//    }
+
+    echo html_writer::start_tag('div', array('class' => 'no-overflow'));
+    $cm->display();
+
+    echo '<p>Here is what you selected previously.</p>';
+    foreach ($data as $id=>$val) {
+        echo "$id is $val";
+        echo '<br>';
+    } 
+
+    echo html_writer::end_tag('div');
+    echo $OUTPUT->footer();
+
 } else {
     $blockname = course_management::_s('blockname');
     $header = course_management::_s('cmcreate');
