5e8222000c6334936ae945560ab9d6aea93dc727
Kevin P. Foote <kpfoote@iup.edu>
2013-03-15 14:53:23 -0400

    Removing empty terms

--- cm_form.php
+++ cm_form.php
@@ -10,7 +10,7 @@ class cm_form extends moodleform {
 
     function definition() {
         global $CFG, $USER; 
-        $CM_DEBUG=TRUE;
+        $CM_DEBUG=FALSE;
 
         $available = course_management::_s('courselist');
         $term_list = course_management::get_term_list('short');
@@ -20,13 +20,20 @@ class cm_form extends moodleform {
         foreach ($term_list as $term) {
             $termname = course_management::get_term_name($term);
             $course_menu = course_management::get_course_list_f($term);
-            $term_heading = '&nbsp;&nbsp<b>Term :: '.$termname.'</b>';
+            $term_heading = '<div style="text-align:left>'
+                           .'&nbsp;&nbsp<b>Term :: '.$termname.'</b>';
             $mform->addElement('html', $term_heading);
-            // if count($course_menu < 1) {}
-            foreach ($course_menu as $id=>$course) {
-                $mform->addElement('advcheckbox', $id, null, $course, array('group'=>(int)$term));
+            if count($course_menu < 1) {
+                $term_nocourse = course_management::_s('nocourse');
+                $mform->addElement('html' , $term_nocourse);
+                $mform->addElement('html' , '</div>'); 
+            } else {
+                foreach ($course_menu as $id=>$course) {
+                    $mform->addElement('advcheckbox', $id, null, $course, array('group'=>(int)$term));
+                }
+                $mform->addElement('html' , '</div>'); 
+                $this->add_checkbox_controller((int)$term);
             }
-            $this->add_checkbox_controller((int)$term);
         }
 
         if($CM_DEBUG) {
