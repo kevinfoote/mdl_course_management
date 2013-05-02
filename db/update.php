281cf556fa54be99c8aa82e30ea39b26e4b53273
Kevin P. Foote <kpfoote@iup.edu>
2013-05-02 11:50:06 -0400

    Add metause field to cm_course table. Fix #919 while Im in here
     - add field cm_course.metause indicator for use in metacourse UI and
       creation
     - add DB indexes on cm_course and cm_enrollment

--- db/update.php
+++ db/update.php
@@ -1,75 +1,44 @@
 <?php
- 
-function xmldb_cm_upgrade($oldversion) {
-    global $CFG;
-    
-    $result = TRUE;
- 
-// Insert PHP code from XMLDB Editor here
-if ($oldversion < 2013020100) {
 
-        // Define table cm_course to be created
+function xmldb_block_course_management_upgrade($oldversion) {
+    global $DB;
+
+    $result = true;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2013042900) {
+
+        // Define field metause to be added to cm_course
         $table = new xmldb_table('cm_course');
+        $field = new xmldb_field('metause', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'active');
 
-        // Adding fields to table cm_course
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('courseshort', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('coursefull', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('instructor', XMLDB_TYPE_CHAR, '100', null, null, null, null);
-        $table->add_field('term', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('active', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
-
-        // Adding keys to table cm_course
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
-        // Conditionally launch create table for cm_course
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
+        // Conditionally launch add field metause
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
         }
+	
+	// Define index cm_course_ix (not unique) to be added to cm_course
+        $table = new xmldb_table('cm_course');
+        $index = new xmldb_index('cm_course_ix', XMLDB_INDEX_NOTUNIQUE, array('courseshort'));
 
-        // Define table cm_enrollment to be created
-        $table = new xmldb_table('cm_enrollment');
+        // Conditionally launch add index cm_course_ix
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+	
+        // Define index cm_instructor_ix (not unique) to be added to cm_course
+        $table = new xmldb_table('cm_course');
+        $index = new xmldb_index('cm_instructor_ix', XMLDB_INDEX_NOTUNIQUE, array('instructor'));
 
-        // Adding fields to table cm_enrollment
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('username', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('courseshort', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-
-        // Adding keys to table cm_enrollment
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
-        // Conditionally launch create table for cm_enrollment
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
+        // Conditionally launch add index cm_instructor_ix
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
         }
 
         // course_management savepoint reached
-        upgrade_block_savepoint(true, 2013020100, 'course_management');
+        upgrade_block_savepoint(true, 2013042900, 'course_management');
     }
 
- if ($oldversion < 2013031100) {
-
-        // Define table cm_term to be created
-        $table = new xmldb_table('cm_term');
-
-        // Adding fields to table cm_term
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('termcode', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('termname', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('active', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
-
-        // Adding keys to table cm_term
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
-        // Conditionally launch create table for cm_term
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-
-        // course_management savepoint reached
-        upgrade_block_savepoint(true, 2013031100, 'course_management');
-    }
- 
     return $result;
 }
-?>
