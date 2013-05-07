e78f88bc9124d96659c429f54aecc9f3ea915a9f
Kevin P. Foote <kpfoote@iup.edu>
2013-05-07 13:57:41 -0400

    version bump

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013050700;        // The current plugin version (Date: YYYYMMDDxx)
+$plugin->version   = 2013050701;        // The current plugin version (Date: YYYYMMDDxx)
 
 $plugin->requires  = 2012120300.00;     // Requires this Moodle version
 
